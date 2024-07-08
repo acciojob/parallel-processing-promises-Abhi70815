8e8728c8d8d31b3df968d499c3a95ebea51fbe68
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-08 22:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", function() {
+  let promises = images.map((image) => {
+    return new Promise((resolve, reject) => {
+      let imgElement = document.createElement("img");
+      imgElement.src = image.url;
+
+      imgElement.onload = () => resolve(imgElement);
+      imgElement.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+    });
+  });
+
+  Promise.all(promises)
+    .then((imgElements) => {
+      imgElements.forEach((imgElement) => {
+        output.appendChild(imgElement);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
